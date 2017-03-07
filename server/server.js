998f54041b117a01f4bf0f3c72d6fc6d28f455b8
Steve Barr <steve.barr@pobox.com>
2017-03-06 19:47:42 -0500

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,6 +15,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user connected');
 
+  socket.emit('newMessage', {
+      from: 'Admin',
+      text: 'Welcome to the chat app',
+      createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+     from: 'Admin',
+     text: 'New user joined',
+     createdAt: new Date().getTime()
+   });
+
+
   // socket.emit('newMessage', {
   //   from: 'mike',
   //   text: 'Hey.  What is going on.',
@@ -27,7 +40,12 @@ io.on('connection', (socket) => {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
-    })
+    });
+    // socket.broadcast.emit('newMessage', {
+    //    from: message.from,
+    //    text: message.text,
+    //    createdAt: new Date().getTime()
+    //  });
   })
 
 
